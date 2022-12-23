ac65794e8d5c043707c5c1625857ec398e7f63c6
ibtehal <ibtehalamro11@gmail.com>
2022-12-23 16:47:27 +0200

    Main structure of the app

---
+++ teaching_centre/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <header>header</header>
+     <main>main</main>
+     <footer>footer</footer>
+    </div>
+  );
+}
+
+export default App;
